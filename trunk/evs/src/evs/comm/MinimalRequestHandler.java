52d62e6ac34bc9787a660628cfa1a4b45e679bca
locutus <locutus@4e363603-e748-0410-94f2-a1644731a11e>
2008-04-08 22:30:56 +0000

    MinimalRequestHandler + build

---
+++ trunk/evs/src/evs/comm/MinimalRequestHandler.java
@@ -0,0 +1,101 @@
+/**
+ * 
+ */
+package evs.comm;
+
+import java.io.DataInput;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.SocketAddress;
+
+/**
+ * @author Gerald Scharitzer (e0127228 at student dot tuwien dot ac dot at)
+ *
+ */
+public class MinimalRequestHandler implements IClientRequestHandler,
+		IServerRequestHandler {
+	
+	private Socket socket;
+	private InputStream socketInputStream;
+	private OutputStream socketOutputStream;
+	
+	public MinimalRequestHandler() {
+		socket = new Socket();
+	}
+
+	/* (non-Javadoc)
+	 * @see evs.comm.IClientRequestHandler#receiveResponse()
+	 */
+	@Override
+	public byte[] receiveResponse() throws RemotingException {
+		DataInput dataInput = new DataInputStream(socketInputStream);
+		int messageLength;
+		try {
+			messageLength = dataInput.readInt();
+		} catch (IOException e) {
+			throw new RemotingException(e);
+		}
+		if (messageLength < 0) {
+			throw new RemotingException("The message length was negative.");
+		}
+		byte[] buffer = new byte[messageLength];
+		try {
+			dataInput.readFully(buffer);
+		} catch (IOException e) {
+			throw new RemotingException(e);
+		}
+		return buffer;
+	}
+
+	/* (non-Javadoc)
+	 * @see evs.comm.IClientRequestHandler#sendRequest(byte[])
+	 */
+	@Override
+	public void sendRequest(byte[] message) throws RemotingException {
+		try {
+			socketOutputStream.write(message);
+		} catch (IOException e) {
+			throw new RemotingException(e);
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see evs.comm.IServerRequestHandler#receiveRequest()
+	 */
+	@Override
+	public byte[] receiveRequest() throws RemotingException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	/* (non-Javadoc)
+	 * @see evs.comm.IServerRequestHandler#sendResponse(byte[])
+	 */
+	@Override
+	public void sendResponse(byte[] message) throws RemotingException {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void bind(SocketAddress localAddress) throws RemotingException {
+		try {
+			socket.bind(localAddress);
+			socketInputStream = socket.getInputStream();
+		} catch (IOException e) {
+			throw new RemotingException(e);
+		}
+	}
+	
+	public void connect(SocketAddress remoteAddress) throws RemotingException {
+		try {
+			socket.connect(remoteAddress);
+			socketOutputStream = socket.getOutputStream();
+		} catch (IOException e) {
+			throw new RemotingException(e);
+		}
+	}
+
+}
