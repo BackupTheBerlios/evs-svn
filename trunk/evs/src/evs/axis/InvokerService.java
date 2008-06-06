40d35b1fdbb4ac03f07d278d859ea324a0e6ad0e
locutus <locutus@4e363603-e748-0410-94f2-a1644731a11e>
2008-06-06 19:09:22 +0000

    axis

---
+++ trunk/evs/src/evs/axis/InvokerService.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package evs.axis;
+
+/**
+ * @author Gerald Scharitzer (e0127228 at student dot tuwien dot ac dot at)
+ *
+ */
+public class InvokerService {
+	
+	public String invoke() {
+		return "result";
+	}
+
+}
